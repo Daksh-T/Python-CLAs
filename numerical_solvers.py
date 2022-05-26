402b2454750b7191e4dc1848efd086bf111bdb67
Daksh-T <dakshtyagi94@gmail.com>
2022-05-26 20:17:57 +0400

    initial commit of combined mirror and lens solvers

---
+++ numerical_solvers.py
@@ -0,0 +1,96 @@
+import time
+import os
+
+def clear():
+    if (os.name == 'posix'):
+        os.system('clear')
+    else:
+        os.system('cls')
+
+clear()
+while 1==1:
+    clear()
+    print("Hello there!")
+    print("What do you want to use today?")
+    print("1. Mirror calculator")
+    print("2. Lens calculator")
+    print("3. Exit")
+    choice = int(input("(1/2/3) "))
+
+
+    if choice == 1:
+        clear()
+        while 1==1:
+            print("Welcome to the mirror calculator")
+            print("Please enter the values with the appropriate sign!")
+            f = input("What is the focal length (f) ? ")
+            u = input("What is the object distance (u)? ")
+            v = input("What is the mirror distance (v)? ")
+            clear()
+
+
+            if v == "":
+                u = float(u)
+                f = float(f)
+                v = (f*u)/(u-f)
+                print("The value of v is ", v)
+            elif u == "":
+                v = float(v)
+                f = float(f)
+                u = (f*v)/(v-f)
+                print("The value of u is ", u)
+            elif f == "":
+                u =float(u)
+                v =float(v)
+                f = (u*v)/(v+u)
+                print("The value of f is ", f)
+            else:
+                print("Invalid input!")
+
+            inp = input("Press enter to restart or q to quit ")
+            if inp == "q":
+                break
+            else:
+                clear()
+
+    elif choice == 2:
+        clear()
+        while 1==1:
+            print("Welcome to the lens calculator")
+            print("Please enter the values with the appropriate sign!")
+            f = input("What is the focal length (f) ? ")
+            u = input("What is the object distance (u)? ")
+            v = input("What is the mirror distance (v)? ")
+            clear()
+
+
+            if v == "":
+                u = float(u)
+                f = float(f)
+                v = (f*u)/(u+f)
+                print("The value of v is ", v)
+            elif u == "":
+                v = float(v)
+                f = float(f)
+                u = (f*v)/(f-v)
+                print("The value of u is ", u)
+            elif f == "":
+                u = float(u)
+                v = float(v)
+                f = (u*v)/(u-v)
+                print("The value of f is ", f)
+            else:
+                print("Invalid input!")
+
+            inp = input("Press enter to restart or q to quit ")
+            if inp == "q":
+                break
+            else:
+                clear()
+    elif choice == 3:
+        print("Goodbye!")
+        time.sleep(1)
+        exit()
+    else:
+        input("Invalid input!")
+        clear()
