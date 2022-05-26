14f30b189ecb915941e65019f3f7d6076be6cda0
Daksh-T <dakshtyagi94@gmail.com>
2022-05-26 20:05:47 +0400

    add while loop to loop program

--- mirror_calculator.py
+++ mirror_calculator.py
@@ -8,30 +8,36 @@ def clear():
         os.system('cls')
     
 clear()
-print("Welcome to the mirror calculator.")
-print("Please enter the values with the appropriate sign!")
-u = input("What is the object distance (u)? ")
-v = input("What is the mirror distance (v)? ")
-f = input("What is the focal length (f) ? ")
-clear()
+
+while 1==1:
+    print("Welcome to the mirror calculator")
+    print("Please enter the values with the appropriate sign!")
+    u = input("What is the object distance (u)? ")
+    v = input("What is the mirror distance (v)? ")
+    f = input("What is the focal length (f) ? ")
+    clear()
 
 
-if v == "":
-    u = float(u)
-    f = float(f)
-    v = (f*u)/(u-f)
-    print("The value of v is ", v)
-elif u == "":
-    v = float(v)
-    f = float(f)
-    u = (f*v)/(v-f)
-    print("The value of u is ", u)
-elif f == "":
-    u =float(u)
-    v =float(v)
-    f = (u*v)/(v+u)
-    print("The value of f is ", f)
-else:
-    print("Invalid input!")
+    if v == "":
+        u = float(u)
+        f = float(f)
+        v = (f*u)/(u-f)
+        print("The value of v is ", v)
+    elif u == "":
+        v = float(v)
+        f = float(f)
+        u = (f*v)/(v-f)
+        print("The value of u is ", u)
+    elif f == "":
+        u =float(u)
+        v =float(v)
+        f = (u*v)/(v+u)
+        print("The value of f is ", f)
+    else:
+        print("Invalid input!")
 
-input("Press enter to close")
+    inp = input("Press enter to restart or q to quit ")
+    if inp == "q":
+        break
+    else:
+        clear()
