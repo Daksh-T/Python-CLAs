da1c9e6f4a6559b9e7cd45cccc2ce9067ff323e9
Daksh-T <dakshtyagi94@gmail.com>
2022-05-26 19:49:40 +0400

    initial commit for mirror calculator

---
+++ mirror_calculator.py
@@ -0,0 +1,35 @@
+import time
+import os
+
+def clear():
+    if (os.name == 'posix'):
+        os.system('clear')
+    else:
+        os.system('cls')
+    
+clear()
+print("Welcome to the mirror calculator.")
+print("Please enter the values with the appropriate sign!")
+u = input("What is the object distance (u)? ")
+v = input("What is the mirror distance (v)? ")
+f = input("What is the focal length (f) ? ")
+clear()
+
+
+if v == "":
+    u = float(u)
+    f = float(f)
+    v = (f*u)/(u-f)
+    print("The value of v is ", v)
+elif u == "":
+    v = float(v)
+    f = float(f)
+    u = (f*v)/(v-f)
+    print("The value of u is ", u)
+elif f == "":
+    u =float(u)
+    v =float(v)
+    f = (u*v)/(v+u)
+    print("The value of f is ", f)
+
+input("Press enter to close")
